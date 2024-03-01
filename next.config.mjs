11ce997b84ca6f2a7f68cbb29bcdf8ffe916c8c5
Eduardo Buratti <eburatti09@gmail.com>
2024-03-01 10:25:45 -0300

    fix deploying into a subpath

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "export",
+  assetPrefix: process.env.NODE_ENV === "development" ? "/" : "/resume",
+  basePath: process.env.NODE_ENV === "development" ? "/" : "/resume",
+  trailingSlash: true,
   compiler: {
     styledComponents: true,
   },
